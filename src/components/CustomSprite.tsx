7ed834481c1a88eb6cb2c65353e957d5ecd72abe
Guillermo <grobles64@ieszaidinvergeles.org>
2025-01-12 15:59:09 +0100

    Botón (no hace nada)

--- src/components/CustomSprite.tsx
+++ src/components/CustomSprite.tsx
@@ -1,4 +1,4 @@
-import { Animated, Easing, StyleSheet, Text, View } from 'react-native'
+import { Animated, Button, Easing } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import { Sprites, type SpritesMethods } from 'react-native-sprites';
 
@@ -7,26 +7,35 @@ const CustomSprite = () => {
     const [positionAnim, setPositionAnim] = useState(new Animated.ValueXY({ x: 0, y: 0 }));
     const spriteRef = useRef<Array<SpritesMethods | null>>([]);
     useEffect(() => {
-        if (spriteRef.current[0] != null)
+        if (spriteRef.current[0] != null) {
             spriteRef.current[0].play({
-                type: 'down',
-                fps: 8,
+                type: 'front',
+                fps: 12,
                 loop: true
             });
-
+        }
         Animated.timing(positionAnim.y, {
-            toValue: 600,
+            toValue: 500,
             easing: Easing.ease,
-            duration: 20000,
+            duration: 5000,
             useNativeDriver: true,
         }).start();
-
+        Animated.timing(positionAnim.x, {
+            toValue: 270,
+            easing: Easing.ease,
+            duration: 5000,
+            useNativeDriver: true,
+        }).start();
     }, [])
 
+    function mover() {
+
+    }
+
     return (
-        <Animated.View
+        <><Animated.View
             style={{
-                transform: [{ translateY: positionAnim.y }]
+                transform: [{ translateY: positionAnim.y }, { translateX: positionAnim.x }]
             }}
         >
             <Sprites
@@ -35,13 +44,17 @@ const CustomSprite = () => {
                 columns={12}
                 rows={4}
                 animations={{
-                    down: { row: 0, startFrame: 0, endFrame: 11 },
-                    left: { row: 1, startFrame: 11, endFrame: 23 },
-                    right: { row: 2, startFrame: 24, endFrame: 35 },
-                    up: { row: 3, startFrame: 36, endFrame: 47 },
-                }}
-            />
-        </Animated.View>
+                    right: { row: 0, startFrame: 0, endFrame: 11 },
+                    front: { row: 1, startFrame: 12, endFrame: 23 },
+                    left: { row: 2, startFrame: 24, endFrame: 35 },
+                    back: { row: 3, startFrame: 36, endFrame: 47 },
+                }} />
+        </Animated.View><Button
+                onPress={mover}
+                title="Learn More"
+                color="#841584"
+                accessibilityLabel="Learn more about this purple button"
+            /></>
     )
 }
 
